f47e1eaced77ecfb047d25be8391eb5cc3135964
madp@fosung.com <mdp101021>
2020-01-14 10:25:27 +0800

    配置docker

---
+++ commom-rst-admin/src/main/java/com/daole/cloud/CommomRstAdminApplication.java
@@ -0,0 +1,13 @@
+package com.daole.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommomRstAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommomRstAdminApplication.class, args);
+    }
+
+}
