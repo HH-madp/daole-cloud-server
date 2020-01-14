f27931531895974491a3116aceb0f4808510437f
madp@fosung.com <mdp101021>
2020-01-14 10:26:01 +0800

    配置docker

---
+++ commom-rst-admin/src/test/java/com/daole/cloud/CommomRstAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daole.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommomRstAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
