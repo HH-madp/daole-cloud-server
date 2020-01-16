8f7c96343cb34be94c03dc71251310144b822ec9
madp@fosung.com <mdp101021>
2020-01-16 16:58:43 +0800

    配置docker

---
+++ commom-rest-admin/src/main/java/com/daole/cloud/common/CommonRestAdminApplication.java
@@ -0,0 +1,13 @@
+package com.daole.cloud.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonRestAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonRestAdminApplication.class, args);
+    }
+
+}
